bfa25926c80b523ba3f695772199d369570ae0e2
Proactii <hello@proactii.com>
2026-02-09 16:04:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Z jobs | Job Search Platform",
+  title: "Z jobs | Job Search ",
   description:
     "Find your dream job with our advanced job search platform. Search by location, job type, and more.",
   metadataBase: new URL("https://zjobs-nine.vercel.app"),
